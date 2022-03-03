08739073ddc97db3a4ad0e2c4cd30f107afe3c4a
DavidTamayoRomo <david95tamayo@otmail.es>
2022-03-03 10:16:25 -0500

    Entrevista inicial IL

--- server/api/v1/entrevista_inicial_ilvem/model.js
+++ server/api/v1/entrevista_inicial_ilvem/model.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const {body}= require('express-validator');
+const { body } = require('express-validator');
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 /*
 const sanitizers = [
   body.apply(title).escape()
@@ -10,74 +10,75 @@ const sanitizers = [
 */
 
 const fields = {
-  fecha:{
-    type : String,
-    require:false,
+  fecha: {
+    type: String,
+    require: false,
   },
-  tiempoCapacitación:{
-    type : String,
-    require:false,
+  estudiantes1:
+    [
+      {
+        observaciones: { type: String },
+        fechaInicio: { type: Date },
+        FechaIncorporacion: { type: Date },
+        tiempoCapacitacion: { type: String },
+        estudiantes: [
+          {
+            idEstudainte: { type: String },
+            nombreEstudiante: { type: String },
+            idDocente: [
+              {
+                item_id: { type: String },
+                nombre: { type: String },
+              }
+            ],
+            idHorario: [
+              {
+                item_id: { type: String },
+                nombre: { type: String },
+              }
+            ],
+          }
+        ]
+      }
+    ],
+  pregunta1: {
+    type: String,
+    require: false,
   },
-  idDocentes:[{
+  pregunta2: {
+    type: String,
+    require: false,
+  },
+  pregunta3: {
+    type: String,
+    require: false,
+  },
+  pregunta4: {
+    type: String,
+    require: false,
+  },
+  pregunta5: {
+    type: String,
+    require: false,
+  },
+  pregunta6: {
+    type: String,
+    require: false,
+  },
+  addedUser: {
     type: Schema.Types.ObjectId,
     ref: 'persona',
-    require:false,
-  }],
-  idHorario:[{
-    type: Schema.Types.ObjectId,
-    ref: 'horario',
-    require:false,
-  }],
-  observaciones:[{
-    type : String,
-    require:false,
-  }],
-  pregunta1:{
-    type : String,
-    require:false,
+    require: false,
   },
-  pregunta2:{
-    type : String,
-    require:false,
-  },
-  pregunta3:{
-    type : String,
-    require:false,
-  },
-  pregunta4:{
-    type : String,
-    require:false,
-  },
-  pregunta5:{
-    type : String,
-    require:false,
-  },
-  pregunta6:{
-    type : String,
-    require:false,
-  },
-  pregunta7:{
-    type : String,
-    require:false,
-  },
-  pregunta8:{
-    type : String,
-    require:false,
-  },
-  addedUser:{
+  modifiedUser: {
     type: Schema.Types.ObjectId,
     ref: 'persona',
-    require:false,
-  },
-  modifiedUser:{
-    type: Schema.Types.ObjectId,
-    ref: 'persona',
-    require:false,
+    require: false,
   },
 };
 
 //timestamps es created at - updated at
-const entrevistainicialilvem = new Schema(fields, {timestamps:true});
+const entrevistainicialilvem = new Schema(fields, { timestamps: true });
 
-module.exports =  mongoose.model('entrevistainicialilvem', entrevistainicialilvem);
+module.exports = mongoose.model('entrevistainicialilvem', entrevistainicialilvem);
 
