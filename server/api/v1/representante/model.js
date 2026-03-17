866f4a0a6ce3f328104f2323ce2d65678f1747bd
MaxAgent <maxagent@local>
2026-03-17 17:39:16 +0000

    Finalize dependency hardening and compatibility fixes

--- server/api/v1/representante/model.js
+++ server/api/v1/representante/model.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const { body } = require('express-validator');
-const mongooseDateFormat = require('mongoose-date-format');
 
 const { Schema } = mongoose;
 /*
@@ -92,9 +91,6 @@ const fields = {
 //timestamps es created at - updated at
 const representante = new Schema(fields, { timestamps: true });
 
-
-//Cambiar formato de fecha formato solo fecha formato YYYY-MM-DD || si se desea cambiar debemos hacer clic + control en mongooseDateFormat 
-representante.plugin(mongooseDateFormat);
 
 module.exports = mongoose.model('representante', representante);
 
